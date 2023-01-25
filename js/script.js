87b27c5ff3b9c5c83f6bde3bc45815dd220870a6
Brendon <brendon.franco@vibryt.com.br>
2023-01-25 20:54:19 -0300

    feat: add some general styles to the page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 import Coundown from "./modules/countdown.js";
 
 const tempoParaONatal = new Coundown("24 December 2023 23:59:59 GMT-0300");
-const tempoParaOAnoNovo = new Coundown("31 December 2023 23:59:59 GMT-0300");
+const countdown = document.querySelectorAll("[data-countdown]");
 
-console.log(tempoParaONatal.total);
-console.log(tempoParaOAnoNovo.total);
+function showCountdownTime() {
+  countdown.forEach((item) => {
+    switch (item.getAttribute("data-countdown")) {
+      case "days":
+        item.innerText = tempoParaONatal.total.days;
+        break;
+      case "hours":
+        item.innerText = tempoParaONatal.total.hours;
+        break;
+      case "minutes":
+        item.innerText = tempoParaONatal.total.minutes;
+        break;
+      default:
+        item.innerText = tempoParaONatal.total.seconds;
+    }
+  });
+}
 
 setInterval(() => {
-  console.log(tempoParaONatal.total);
-  console.log(tempoParaOAnoNovo.total);
+  showCountdownTime();
 }, 1000);
+
+showCountdownTime();
